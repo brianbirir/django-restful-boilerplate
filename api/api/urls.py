491cd83f43ccc92120b8d6f7458d3ef911d5a59d
Brian Birir <brianbirir@gmail.com>
2023-07-22 16:15:10 +0300

    chore: add drf

--- api/api/urls.py
+++ api/api/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from rest_framework import routers, serializers, viewsets
+
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    re_path(r'^api/', include('users.urls')),
 ]
